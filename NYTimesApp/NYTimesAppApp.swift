a02116da058d3ea308ab1db65a3a0c8b42ed3868
Dmytro <dmytro.besedin@gmail.com>
2023-07-08 20:16:19 +0300

    Initial Commit

---
+++ NYTimesApp/NYTimesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesAppApp.swift
+//  NYTimesApp
+//
+//  Created by Dmytro Besedin on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
